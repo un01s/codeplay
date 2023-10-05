2bdfe60e5510b429de98371c9482abb260898c62
un01s <96022876+un01s@users.noreply.github.com>
2023-10-05 08:09:27 -0600

    update

--- backtrack/0078-subset.cpp
+++ backtrack/0078-subset.cpp
@@ -59,3 +59,20 @@ public:
         return res;
     }
 };
+
+// iterative
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> res = {{}};
+        for (int num : nums) {
+            int n = res.size();
+            for (int i = 0; i < n; i++) {
+                res.push_back(res[i]); 
+                res.back().push_back(num);
+            }
+        }
+        return res;    
+    }
+};
+

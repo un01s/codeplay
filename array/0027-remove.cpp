37de2bb6376f9847fc22e84408c992b8d3cc70bb
un01s <96022876+un01s@users.noreply.github.com>
2023-10-10 12:29:55 -0600

    update

---
+++ array/0027-remove.cpp
@@ -0,0 +1,21 @@
+/*
+ * leetcode 27
+ * remove element
+ *
+ * double-pointer: fast-slow
+ *
+ */
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int slow = 0;
+        for (int fast = 0; fast < nums.size(); fast++) {
+            if (val != nums[fast]) {
+                nums[slow++] = nums[fast];
+            }
+        }
+        return slow;
+    }
+};
+

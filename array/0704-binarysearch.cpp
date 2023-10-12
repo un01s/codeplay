f680b636475e98872471c3897e9eeafa9568bdab
un01s <96022876+un01s@users.noreply.github.com>
2023-10-12 11:03:37 -0600

    update

--- array/0704-binarysearch.cpp
+++ array/0704-binarysearch.cpp
@@ -3,6 +3,9 @@
  * binary search
  */
 
+// 
+// the search range: [l, r]
+//
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
@@ -14,6 +17,28 @@ public:
                 l = m+1;
             } else if (nums[m] > target) {
                 r = m-1;
+            } else {
+                return m;
+            }
+        }
+        return -1;
+    }
+};
+
+//
+// search range: [l, r)
+//
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int l = 0, r = n;
+        while (l < r) {
+            int m = l + (r-l)/2;
+            if (nums[m] < target) {
+                l = m+1;
+            } else if (nums[m] > target) {
+                r = m;
             } else {
                 return m;
             }

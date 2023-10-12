d05ebae728bfb8dc4c978a505ffb6a6e543e5b30
un01s <96022876+un01s@users.noreply.github.com>
2023-10-12 12:14:13 -0600

    update

--- array/0287-findDuplicate.cpp
+++ array/0287-findDuplicate.cpp
@@ -25,6 +25,22 @@ public:
 };
 
 //
+// unordered_map
+//
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        unordered_map<int, bool> mp;
+        for (int n : nums) {
+            if (mp[n]) return n;
+            mp[n] = true;
+        }
+        return -1;    
+    }
+};
+
+
+//
 // use the functions of unordered_set
 // find()
 // insert()

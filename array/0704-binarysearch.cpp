5c314dfb105cc31a71087b7a06b70c4468a2f3f2
un01s <96022876+un01s@users.noreply.github.com>
2023-10-12 11:00:42 -0600

    update

---
+++ array/0704-binarysearch.cpp
@@ -0,0 +1,24 @@
+/*
+ * leetcode 704
+ * binary search
+ */
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int l = 0, r = n - 1;
+        while (l <= r) {
+            int m = l + (r-l)/2;
+            if (nums[m] < target) {
+                l = m+1;
+            } else if (nums[m] > target) {
+                r = m-1;
+            } else {
+                return m;
+            }
+        }
+        return -1;
+    }
+};
+

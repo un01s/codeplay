404f120dcc8d88e45aecb99f07c3e9e6a1259457
un01s <96022876+un01s@users.noreply.github.com>
2023-10-12 12:23:29 -0600

    update

--- array/0287-findDuplicate.cpp
+++ array/0287-findDuplicate.cpp
@@ -83,3 +83,26 @@ public:
         return left;       
     }
 };
+
+//
+// fast-slow double-pointer
+//
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[0];
+        int fast = nums[nums[0]];
+        while (slow!=fast)
+        {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        fast = 0;
+        while (slow!=fast)
+        {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;       
+    }
+};

0aab83a2a74cd0a788c776899b4c6280cb709944
un01s <96022876+un01s@users.noreply.github.com>
2023-10-12 20:27:07 -0600

    update

--- dp/0746-mincost.cpp
+++ dp/0746-mincost.cpp
@@ -10,6 +10,21 @@
  * 
  */
 
+// easier
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        vector<int> dp(n+1, 0);
+        for (int i = 0; i <= n; i++) {
+            if (i < 2) dp[i] = cost[i];
+            else dp[i] = cost[i] + min(dp[i-1], dp[i-2]);
+        }
+        return min(dp[n-1], dp[n-2]);
+    }
+};
+
+
 // easier to understand
 class Solution {
 public:
